syrkles420/frameworks_av

camera: Allow devices to load custom CameraParameter code
* Some devices need additional code to load their cameras. Add a hook
for extra classes and symbols to be included.

Originally implemented in Android.mk at
38b27bf2e5079441358288402b38865ba7c1d9d6
Credit to Ethan Chen for original Android.mk implementation

Change-Id: Iec667af2f0bfdb41f14df342cb128908244d7af1

camera/parameters.go
@@ -1,0 +1,52 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package lineage;
+
+import (
+  "android/soong/android"
+  "android/soong/cc"
+)
+
+func init() {
+  android.RegisterModuleType("camera_parameters_defaults", cameraParametersFactory)
+}
+
+func cameraParametersFactory() android.Module {
+  module := cc.DefaultsFactory()
+  android.AddLoadHook(module, cameraParameters)
+
+  return module
+}
+
+func cameraParameters(ctx android.LoadHookContext) {
+  type props struct {
+    Whole_static_libs []string
+  }
+
+  p := &props{}
+  p.Whole_static_libs = globalDefaults(ctx)
+
+  ctx.AppendProperties(p)
+}
+
+func globalDefaults(ctx android.BaseContext) ([]string) {
+  var staticLibs []string
+
+  device_camera_parameters_lib := ctx.DeviceConfig().SpecificCameraParametersLibrary()
+  if (len(device_camera_parameters_lib) > 0) {
+    staticLibs = append(staticLibs, device_camera_parameters_lib)
+  }
+
+  return staticLibs
+}

syberia-project/platform_frameworks_av

Signed-off-by: DennySPB <dennyspb@gmail.com>

camera/parameters.go
@@ -1,0 +1,52 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package Syberia;
+
+import (
+  "android/soong/android"
+  "android/soong/cc"
+)
+
+func init() {
+  android.RegisterModuleType("camera_parameters_defaults", cameraParametersFactory)
+}
+
+func cameraParametersFactory() android.Module {
+  module := cc.DefaultsFactory()
+  android.AddLoadHook(module, cameraParameters)
+
+  return module
+}
+
+func cameraParameters(ctx android.LoadHookContext) {
+  type props struct {
+    Whole_static_libs []string
+  }
+
+  p := &props{}
+  p.Whole_static_libs = globalDefaults(ctx)
+
+  ctx.AppendProperties(p)
+}
+
+func globalDefaults(ctx android.BaseContext) ([]string) {
+  var staticLibs []string
+
+  device_camera_parameters_lib := ctx.DeviceConfig().SpecificCameraParametersLibrary()
+  if (len(device_camera_parameters_lib) > 0) {
+    staticLibs = append(staticLibs, device_camera_parameters_lib)
+  }
+
+  return staticLibs
+}