47d20c0d36b178e35537051885bc08344da7ab03
Praveen Chavan <pchavan@codeaurora.org>
2020-10-15 10:40:41 -0700

    EXPERIMENTAL: CCodec: Add metadata buffer support for linear output buffers
    For secure linear output buffers, relay the handle as-is,
    flattened into a metadata payload for native mediacodec
    clients (who know how to deal with it)
    
    CRs-Fixed: 2812172
    
    Change-Id: I7dacb117d34c0a81af805aaa2cd0b40195c4d6f4

--- media/codec2/sfplugin/CCodecBuffers.cpp
+++ media/codec2/sfplugin/CCodecBuffers.cpp
@@ -1386,6 +1386,15 @@ std::function<sp<Codec2Buffer>()> LinearOutputBuffers::getAlloc() {
     };
 }
 
+// LinearMetadataOutputBuffers
+
+std::function<sp<Codec2Buffer>()> LinearMetadataOutputBuffers::getAlloc() {
+    return [format = mFormat]{
+        // TODO: proper max output size
+        return new LocalLinearMetadataBuffer(format, new ABuffer(kLinearBufferSize));
+    };
+}
+
 // GraphicOutputBuffers
 
 sp<Codec2Buffer> GraphicOutputBuffers::wrap(const std::shared_ptr<C2Buffer> &buffer) {
