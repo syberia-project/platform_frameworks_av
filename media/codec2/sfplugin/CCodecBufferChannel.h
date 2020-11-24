680fe614b46033a7ae705176494a476527c66438
Wonsik Kim <wonsik@google.com>
2020-11-24 15:01:33 -0800

    CCodec: support audio encoders requesting frame size
    Bug: 150001473
    Test: atest ccodec_unit_test
    Merged-In: I323a55a1dd9b042a1b46dbeb2b84cae32ed48ebe
    Change-Id: I323a55a1dd9b042a1b46dbeb2b84cae32ed48ebe
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- media/codec2/sfplugin/CCodecBufferChannel.h
+++ media/codec2/sfplugin/CCodecBufferChannel.h
@@ -32,6 +32,7 @@
 #include <media/stagefright/CodecBase.h>
 
 #include "CCodecBuffers.h"
+#include "FrameReassembler.h"
 #include "InputSurfaceWrapper.h"
 #include "PipelineWatcher.h"
 
@@ -273,6 +274,8 @@ private:
         size_t numExtraSlots;
         uint32_t inputDelay;
         uint32_t pipelineDelay;
+
+        FrameReassembler frameReassembler;
     };
     Mutexed<Input> mInput;
     struct Output {
