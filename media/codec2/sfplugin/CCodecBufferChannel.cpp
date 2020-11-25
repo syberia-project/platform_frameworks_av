48a187d87affe73475a658c74f933fbe2744da57
Brijesh Patel <pbrijesh@codeaurora.org>
2020-11-25 12:07:00 +0530

    codec2: Avoid invalid access to output format while print
    - Once codec updates output format to client for encode usecase
      which can later be modified. After calling onOutputBufferAvailable
      codec2 should assume that it is not the only owner format object
      and should avoid debug print which can dereference invalid objects.
    
    CRs-Fixed: 2796867
    Change-Id: I441c59f704306fa3aeece14043d2cc0c43da8fc5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -1984,7 +1984,10 @@ bool CCodecBufferChannel::handleWork(
             outBuffer->meta()->setInt64("timeUs", timestamp.peek());
             outBuffer->meta()->setInt32("flags", BUFFER_FLAG_CODEC_CONFIG);
             ALOGV("[%s] onWorkDone: csd index = %zu [%p]", mName, index, outBuffer.get());
-
+            if (outputFormat) {
+                ALOGD("[%s] sending CSD : output format changed to %s",
+                      mName, outputFormat->debugString().c_str());
+            }
             output.unlock();
             mCallback->onOutputBufferAvailable(index, outBuffer);
         } else {
@@ -2037,7 +2040,7 @@ bool CCodecBufferChannel::handleWork(
                 notifyClient,
                 timestamp.peek(),
                 flags,
-                outputFormat,
+                (initData == nullptr ? outputFormat : nullptr),
                 worklet->output.ordinal);
     }
     sendOutputBuffers();
