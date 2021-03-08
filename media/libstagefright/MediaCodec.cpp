473c55f30d6ad1a139916c5ce07acb3462c98208
Wonsik Kim <wonsik@google.com>
2021-03-08 14:24:07 -0800

    stagefright: fix stop-error-error crash
    Bug: 181194470
    Test: atest mediacodecTest
    Change-Id: I5d0c7b2ada410c160328fd5ad01adb6dff0f6a3a

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -2229,6 +2229,8 @@ void MediaCodec::onMessageReceived(const sp<AMessage> &msg) {
                                 }
                                 postPendingRepliesAndDeferredMessages(origin + ":dead");
                                 sendErrorResponse = false;
+                            } else if (!mReplyID) {
+                                sendErrorResponse = false;
                             }
                             break;
                         }
