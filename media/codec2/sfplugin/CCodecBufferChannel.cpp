a482c3fd29ab4666a0be1ff6ce1b8f4eb14aded7
mtk11204 <wenxue.zhang@mediatek.com>
2022-06-22 17:49:04 +0800

    fix flushed work shouldn't request input
    There is work done after flush completed.
    That reqeusts all the input, leading resume requesting input fail
    and report error to app. If the work of onWorkDone is after flush,
    that should not request input.
    
    Bug: 236940478
    Test: Video Playback
    
    Change-Id: Ic7e83b584e321f3dc98a74a0243b6901efc9a625
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -1725,6 +1725,7 @@ bool CCodecBufferChannel::handleWork(
 
     // Whether the output buffer should be reported to the client or not.
     bool notifyClient = false;
+    bool isFlushedWork = false;
 
     if (work->result == C2_OK){
         notifyClient = true;
@@ -1736,6 +1737,7 @@ bool CCodecBufferChannel::handleWork(
             return false; // omitted
         }
         ALOGD("[%s] flushed work; ignored.", mName);
+        isFlushedWork = true;
     } else {
         // C2_OK and C2_NOT_FOUND are the only results that we accept for processing
         // the config update.
@@ -2048,6 +2050,11 @@ bool CCodecBufferChannel::handleWork(
                 worklet->output.ordinal);
     }
     sendOutputBuffers();
+
+    if (isFlushedWork) {
+        return false;
+    }
+
     return true;
 }
 
