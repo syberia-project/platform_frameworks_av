938db2879b1389f68ba099a033711db1292f3c95
DennySPb <DennySPb@gmail.com>
2023-12-07 11:18:25 +0300

    Revert "Camera: Wait for the status tracker idle fence"
    This reverts commit 7a6c0706e81daa02b3d1d6c28441079f15b83e80.

--- services/camera/libcameraservice/device3/StatusTracker.cpp
+++ services/camera/libcameraservice/device3/StatusTracker.cpp
@@ -172,7 +172,6 @@ bool StatusTracker::threadLoop() {
         }
     }
 
-    bool waitForIdleFence = false;
     // After new pending states appear, or timeout, check if we're idle.  Even
     // with timeout, need to check to account for fences that may still be
     // clearing out
@@ -197,7 +196,6 @@ bool StatusTracker::threadLoop() {
             ssize_t idx = mStates.indexOfKey(newState.id);
             // Ignore notices for unknown components
             if (idx >= 0) {
-                bool validFence = newState.fence != Fence::NO_FENCE;
                 // Update single component state
                 mStates.replaceValueAt(idx, newState.state);
                 mIdleFence = Fence::merge(String8("idleFence"),
@@ -206,8 +204,6 @@ bool StatusTracker::threadLoop() {
                 ComponentState newState = getDeviceStateLocked();
                 if (newState != prevState) {
                     mStateTransitions.add(newState);
-                } else if (validFence && !waitForIdleFence) {
-                    waitForIdleFence = true;
                 }
                 prevState = newState;
             }
@@ -230,13 +226,6 @@ bool StatusTracker::threadLoop() {
         }
     }
     mStateTransitions.clear();
-
-    if (waitForIdleFence) {
-        auto ret = mIdleFence->wait(kWaitDuration);
-        if (ret == NO_ERROR) {
-            mComponentsChanged = true;
-        }
-    }
 
     return true;
 }
